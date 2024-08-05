c3e1333a069c2f35bd5bfc33d2c058ad3ac8a508
kinalramani <kinalramani14@gmail.com>
2024-08-05 11:50:56 +0530

    commit by kinal

--- E_DanceStudio/settings.py
+++ E_DanceStudio/settings.py
@@ -130,13 +130,16 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "makvananickfun@gmail.com"
-EMAIL_HOST_PASSWORD = "uchbesumbjxthwyh"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 
 # settings.py
